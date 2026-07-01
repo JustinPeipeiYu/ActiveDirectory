1c896b02737a5937739fd5ed59c5004d1caf5f38
peipei <peipeiyu00@gmail.com>
2026-07-01 19:40:23 -0400

    first commit

---
+++ New-ADUser.ps1
@@ -0,0 +1,17 @@
+﻿$name = Read-Host "Enter Full Name" -AsSecureString
+#$gname = Read-Host "Enter Given Name" -AsSecureString
+#$sname = Read-Host "Enter Surname" -AsSecureString
+#$pwd = Read-Host "Enter Password" -AsSecureString
+#Get-Content scriptname.ps1 | Set-Clipboard
+
+New-ADUser `
+-Name "Jean Luc Picard" `
+-GivenName "Jean" `
+-Surname "Picard" `
+-SamAccountName "jlpicard" `
+-UserPrincipalName "jlpicard@yourdomain.local" `
+-Path "OU=Users,DC=yourdomain,DC=local" `
+-AccountPassword ($pwd) `
+-ChangePasswordAtLogon $true `
+-PasswordNeverExpires $true `
+-Enabled $true `
